d575a67459bbbe01787a94d6e4744fd301d92a40
Simon <simon@balz.me>
2018-09-20 09:59:50 +0200

    Added images of my own favorite radio stations

--- directory/images.js
+++ directory/images.js
@@ -7,10 +7,12 @@ module.exports = {
   teufel2: 'https://u.imageresize.org/v2/63e173e6-c857-453c-9309-4e9a743f211f.jpeg',
   folderIcon: 'https://neeo-sdk.neeo.io/folder.jpg',
   fileIcon: 'https://neeo-sdk.neeo.io/file.jpg',
-  
+
   // Radio stations
-  antenne1: 'https://u.imageresize.org/v2/311061e8-b4ec-43a4-bb48-0d1c4e853950.jpeg', 
-  // chillout: 'http://d1i6vahw24eb07.cloudfront.net/s96745q.png', 
+  bern1: 'https://u.imageresize.org/v2/cf687747-2ed0-4a7b-ad50-b06b6342dee1.jpeg',
+  srf3: 'https://u.imageresize.org/v2/549488b9-d66b-4735-9a06-c21fe6531506.png',
+  antenne1: 'https://u.imageresize.org/v2/311061e8-b4ec-43a4-bb48-0d1c4e853950.jpeg',
+  // chillout: 'http://d1i6vahw24eb07.cloudfront.net/s96745q.png',
   chillout: 'https://u.imageresize.org/v2/2675a3aa-268b-4a41-bd72-50f9c2741a81.jpeg',
   rtlBerlin: 'https://u.imageresize.org/v2/bb991b3a-bc06-42ab-8459-7270ade675fd.jpeg',
   // swr3:  'http://d1i6vahw24eb07.cloudfront.net/s24896q.png',
